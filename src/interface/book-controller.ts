ac1a1256c3d67e1967b6fd3bd81a874eaa459b08
Olga Mahin <olgamahin@hotmail.com>
2024-09-16 19:10:42 -0300

    git commit -m :recycle: refactor: code refactored and adding requisitions

--- src/interface/book-controller.ts
+++ src/interface/book-controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 import { CreateBookUseCase } from '../application/use-cases/create-book-use-case';
 import { ListAllBooksUseCase } from '../application/use-cases/list-all-books-use-case';
-
 export interface CreateBookDTO {
   title: string;
   author: string;
@@ -25,5 +24,19 @@ interface BookDTO {
 }
 
 export class BookController {
-  //seu codigo aqui
+  constructor(
+    private createBookUseCase: CreateBookUseCase,
+    private listAllBooksUseCase: ListAllBooksUseCase
+  ) {}
+  create(req: Request, res: Response): Response {
+    const bookDTO: CreateBookDTO = req.body;
+    const book = this.createBookUseCase.execute(bookDTO);
+    return res.status(201).json(book);
+  }
+
+  listAll(req: Request, res: Response): Response {
+    const books = this.listAllBooksUseCase.execute();
+    return res.status(200).json(books);
+  }
+
 }
